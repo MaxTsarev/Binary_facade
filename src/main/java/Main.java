ff7e44e8d9e8041682da559bc50fb1d12191d9ef
MaxTsarev <maxtsar33@gmail.com>
2023-05-25 19:23:21 +0300

    Added project

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        BinOps binOps = new BinOps();
+
+        System.out.println(binOps.sum("1010", "10100"));
+        System.out.println(binOps.mult("11001", "11110"));
+    }
+}
